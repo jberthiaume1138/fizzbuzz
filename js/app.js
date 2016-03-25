01a28a9fce67b1253a6b448ee83fd3dacd883734
Joseph Berthiaume <joenyc23@gmail.com>
2016-03-25 14:09:26 -0400

    refactoring

--- js/app.js
+++ js/app.js
@@ -1,64 +1,27 @@
 document.getElementById("btn-go").addEventListener("click",function(event) {
 	var endPoint = parseInt(document.getElementById("input-number").value,10);
-	console.log("my endpoint is " + endPoint);
-	//fizzBuzz(endPoint);
-	if(isNaN(endPoint)){
-		console.log("Bad input. endPoint is " + endPoint);
-		alert("you must enter an intger value!");
-	}
-	else {
-		console.log("success. endPoint = " + endPoint);
-		fizzBuzz(endPoint);
-	}
-
-
-	
+	fizzBuzz(validateInput(endPoint));
 });
 
 document.getElementById("input-number").addEventListener("keydown",function(event) {
 	if (event.keyCode == 13) {
-		// console.log(this.value);
-		var endPoint = parseInt(document.getElementById("input-number").value,10);
-		console.log("the endPoint with ENTER is " + endPoint);
-		fizzBuzz(endPoint);
+		fizzBuzz(validateInput(parseInt(this.value,10)));
 	}
 });
 
-
-// $(document).ready(function() {
-
-// 	// var myArray = new Array();
-// 	var endPoint;
-	
-// 	// $('#btn-go').on('click',function() {
-// 	// 	endPoint=$('#input-number').val().trim();
-// 	// 	var myEndPoint = document.getElementById("input-number").value;
-// 	// 	console.log("my endpoint is" + myEndPoint);
-// 	// 	fizzBuzz(endPoint);
-// 	// });
-
-
-// 	//rewrite this with straight javascript only
-// 	// var myEndPoint = document.getElementById('#input-number');
-// 	// console.log(myEndPoint);
-// 	// document.getElementById('#btn-go').onclick
-
-// 	// ----- end get value from box
-
-// 	// $('#input-number').keydown(function(e) {
-// 	// 	if (e.keyCode == 13) {
-// 	//  		endPoint = $('#input-number').val().trim();
-// 	//  		fizzBuzz(endPoint);
-// 	// 	}
-// 	// });
-
-// 	//rewrite this with straight javascript only
-// });
-
-
+function validateInput(endPoint) {
+	if (isNaN(endPoint)) {
+		alert("you must enter an integer value! Try again.");
+		document.getElementById("input-number").value = "";
+	}
+	else {
+		return endPoint;
+	}
+};
 
 function fizzBuzz(endPoint) {
-		$('#numbers').empty(); 		//reset the list 
+		document.getElementById("numbers").value = "";
+		// $('#numbers').empty(); 		//reset the list 
 		var myArray = new Array();
     	for (var i = 1; i <= endPoint; i++) {
 	 		myArray.push(i); //generates the array 1 -  var end
@@ -91,3 +54,40 @@ function fizzBuzz(endPoint) {
 
 	 	// there are more efficient ways 
 };
+
+
+
+
+
+
+// -------------------- jQuery way -------------------------------
+// $(document).ready(function() {
+
+// 	// var myArray = new Array();
+// 	var endPoint;
+	
+// 	// $('#btn-go').on('click',function() {
+// 	// 	endPoint=$('#input-number').val().trim();
+// 	// 	var myEndPoint = document.getElementById("input-number").value;
+// 	// 	console.log("my endpoint is" + myEndPoint);
+// 	// 	fizzBuzz(endPoint);
+// 	// });
+
+
+// 	//rewrite this with straight javascript only
+// 	// var myEndPoint = document.getElementById('#input-number');
+// 	// console.log(myEndPoint);
+// 	// document.getElementById('#btn-go').onclick
+
+// 	// ----- end get value from box
+
+// 	// $('#input-number').keydown(function(e) {
+// 	// 	if (e.keyCode == 13) {
+// 	//  		endPoint = $('#input-number').val().trim();
+// 	//  		fizzBuzz(endPoint);
+// 	// 	}
+// 	// });
+// });
+
+
+
